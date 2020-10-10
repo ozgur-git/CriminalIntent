50362465196bf73dfc4c0c49ab9f8fc2809d8f77
ozgur-sys <ozgursevik@gmail.com>
2020-10-10 06:33:44 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -1,4 +1,44 @@
 package com.example.criminalintent;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
 }
