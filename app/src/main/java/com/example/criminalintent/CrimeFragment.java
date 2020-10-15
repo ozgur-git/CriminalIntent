c6bf4ee154736bcfb6bd36a8de028534fc12a931
ozgur-sys <ozgursevik@gmail.com>
2020-10-15 06:58:18 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -1,17 +1,62 @@
 package com.example.criminalintent;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.Date;
 
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime=new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable  Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_crime,container,false);
+        //TODO false dene
+//       View v=View.inflate(this,R.layout.fragment_crime,null);
+        //TODO view dene
+        mDateButton=v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mTitleField=v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTitleField.setText(s);
+                //TODO s.toString() dene
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
     }
 }
