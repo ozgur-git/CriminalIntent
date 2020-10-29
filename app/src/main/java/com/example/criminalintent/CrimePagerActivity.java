6cbc1ea7f3d5f87ea452ff7d3ef8433c53af9abc
ozgur-sys <ozgursevik@gmail.com>
2020-10-29 13:10:53 +0300

    version 0.1

---
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,13 @@
+package com.example.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
