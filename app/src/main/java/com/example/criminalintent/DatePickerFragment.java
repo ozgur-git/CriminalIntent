b16c3f6027bf1cc22f59b76e55bbf829181b5628
ozgur-sys <ozgursevik@gmail.com>
2020-11-01 10:43:23 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/criminalintent/DatePickerFragment.java
@@ -3,6 +3,8 @@ package com.example.criminalintent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -12,8 +14,15 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+//        View v=getLayoutInflater().inflate(R.layout.dialog_date,null);
+//        View view= LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date,null);
+
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.date_picker_title)
+                .setView(R.layout.dialog_date)
+//                .setView(view)
                 .setPositiveButton(android.R.string.ok,null).create();
+
     }
 }
