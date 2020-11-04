40985924f90e85c9bc68159889a13c21272879b0
ozgur-sys <ozgursevik@gmail.com>
2020-11-04 07:10:49 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/criminalintent/DatePickerFragment.java
@@ -6,7 +6,9 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.DatePicker;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -41,6 +43,12 @@ public class DatePickerFragment extends DialogFragment {
         fragment.setArguments(args);
 
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @NonNull
---
+++ app/src/main/java/com/example/criminalintent/DatePickerFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.example.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class DatePickerFragmentActivity extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
