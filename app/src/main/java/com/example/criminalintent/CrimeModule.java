f4b9f7b4fa1a128cc2c6ddb0b75e79394f7b3849
ozgur-sys <ozgursevik@gmail.com>
2020-11-11 06:32:10 +0300

    version 0.1

---
+++ app/src/main/java/com/example/criminalintent/CrimeModule.java
@@ -0,0 +1,36 @@
+package com.example.criminalintent;
+
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+
+@Module
+public class CrimeModule {
+
+    @Provides
+    Crime provideCrime(){
+
+        return new Crime();
+    }
+
+    @Provides
+    @Singleton
+    List<Crime> providesCrimeList(){
+
+        List<Crime> mCrimes = new ArrayList<>();
+        for(int i=0;i<5;i++)
+        {
+            Crime crime=new Crime();
+            crime.setTitle("Crime #"+(i+1));
+            crime.setRequiresPolice((1==(i%3)));
+            crime.setSolved(1==(i%2));
+            mCrimes.add(crime);
+        }
+
+        return mCrimes;
+    }
+
+}
