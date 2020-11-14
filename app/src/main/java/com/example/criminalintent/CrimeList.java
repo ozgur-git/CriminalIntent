6405cec77736ab76f5028ecb0a04e730350e2e22
ozgur-sys <ozgursevik@gmail.com>
2020-11-14 09:25:24 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/CrimeList.java
+++ app/src/main/java/com/example/criminalintent/CrimeList.java
@@ -3,12 +3,15 @@ package com.example.criminalintent;
 import android.content.Context;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CrimeList {
 
-    CrimeComponent mComponent;
+    @Inject
+    Provider<Crime> obj;
+
     List<Crime> mCrimes = new ArrayList<>();
 
     static int count=0;
@@ -19,7 +22,9 @@ public class CrimeList {
 
         for(int i=0;i<7;i++)
         {
-            Crime newCrime=new Crime();
+
+            Crime newCrime=obj.get();
+//            Crime newCrime=new Crime();
             newCrime.setTitle("Crime #"+(i+1));
             newCrime.setRequiresPolice((1==(i%3)));
             newCrime.setSolved(1==(i%2));
