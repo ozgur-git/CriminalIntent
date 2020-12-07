136517c79bcae221c682cc69a54a98b77a2dbce3
ozgur-sys <ozgursevik@gmail.com>
2020-12-07 06:52:28 +0300

    version 0.1

---
+++ app/src/main/java/com/example/criminalintent/ZoomFragmentActivity.java
@@ -0,0 +1,11 @@
+package com.example.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class ZoomFragmentActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
