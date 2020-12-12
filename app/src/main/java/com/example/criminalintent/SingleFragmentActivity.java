422df3bdd0c85ebd208ac7a93ff1637011eded01
ozgur-sys <ozgursevik@gmail.com>
2020-12-12 09:46:08 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
@@ -1,19 +1,27 @@
 package com.example.criminalintent;
 
 import android.os.Bundle;
+import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
-    protected abstract Fragment createFragment();
+    @LayoutRes
+    private int getLayoutResId(){
 
+        return R.layout.activity_fragment;
+    }
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+
+        setContentView(getLayoutResId());
 
         if  (savedInstanceState==null) {
 
