03f23e41e171878fe5c3a9099e3861f0df161311
ozgur-sys <ozgursevik@gmail.com>
2020-12-19 08:40:28 +0300

    version 0.1

--- app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
@@ -1,12 +1,18 @@
 package com.example.criminalintent;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import java.util.logging.Logger;
+
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    Logger mLogger=Logger.getLogger(getClass().getName());
 
     @LayoutRes
     private int getLayoutResId(){
@@ -28,6 +34,16 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, createFragment()).commit();
         }
+
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics ();
+        display.getMetrics(outMetrics);
+
+        float density  = getResources().getDisplayMetrics().density;
+        float dpHeight = outMetrics.heightPixels ;
+        float dpWidth  = outMetrics.widthPixels / density;
+
+        mLogger.info("height is "+dpHeight+" width is "+dpWidth);
 
     }
 }
